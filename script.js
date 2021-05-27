4f585e6cd793df38f40ae4afb0aac0d04a2c876f
Glitch (glitch-hello-website) <none>
2021-05-27 05:33:18 +0000

    😚🖐 Checkpoint ./style.css:15037470/1069 ./index.html:15037470/315 ./script.js:15037470/1162

--- script.js
+++ script.js
@@ -2,4 +2,13 @@ $( document ).ready(function() {
   $('.mirror').on('keyup', function() {
     $('.'+$(this).attr('class')).val($(this).val());
 });
+  
+ Array.from( document.querySelectorAll('[data-expand]'), (input)=>{
+   let parent = input.parentNode;
+   function updateSize(){
+     parent.dataset.value = input.value
+   }
+   input.addEventListener('input', updateSize);
+   updateSize();
+ });  
 });
