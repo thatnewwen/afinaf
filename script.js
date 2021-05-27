87c6f3cfcc4614a1acabc6d8f17b307fc8f80f3e
Glitch (glitch-hello-website) <none>
2021-05-27 05:57:20 +0000

    🌵📇 Checkpoint ./index.html:15037470/736 ./style.css:15037470/2240 ./script.js:15037470/552

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 $( document ).ready(function() {
   $('.mirror').on('keyup', function() {
-    $('.'+$(this).attr('class')).val($(this).val());
+    $('.'+$(this).attr('class')).text($(this).text());
+    $(this).focus()
 });
   
- Array.from( document.querySelectorAll('[data-expand]'), (input)=>{
-   let parent = input.parentNode;
-   function updateSize(){
-     parent.dataset.value = input.value
-   }
-   input.addEventListener('input', updateSize);
-   updateSize();
- });  
+ // Dealing with Input width
+let el = document.querySelector(".input-wrap .input");
+let widthMachine = document.querySelector(".input-wrap .width-machine");
+el.addEventListener("keyup", () => {
+  widthMachine.innerHTML = el.value;
+});
 });
