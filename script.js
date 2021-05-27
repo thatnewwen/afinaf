f07e5d2a4ca1bbcce4621c2147e1d4760543275f
Glitch (glitch-hello-website) <none>
2021-05-27 06:06:16 +0000

    🔖🎨 Checkpoint ./script.js:15037470/1889 ./style.css:15037470/241

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 $( document ).ready(function() {
-  $('.mirror').on('keyup', function() {
-    $('.'+$(this).attr('class')).text($(this).text());
-    $(this).focus()
-});
+  function delay(callback, ms) {
+  var timer = 0;
+  return function() {
+    var context = this, args = arguments;
+    clearTimeout(timer);
+    timer = setTimeout(function () {
+      callback.apply(context, args);
+    }, ms || 0);
+  };
+}
+  $('.mirror').keyup(delay(function (e) {
+  $('.'+$(this).attr('class')).text($(this).text());
+}, 500));
   
  // Dealing with Input width
 let el = document.querySelector(".input-wrap .input");
