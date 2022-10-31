cc22a52dfcdb0da1cdfe48e04dd79f49e24426f2
Glitch (glitch-hello-website) <none>
2022-10-31 03:51:42 +0000

    ❣️👓 Checkpoint ./canvas/index.html:15037470/24 ./canvas/script.js:15037470/468

---
+++ canvas/script.js
@@ -0,0 +1,15 @@
+$( document ).ready(function() {
+  const canvas = document.getElementById('canvas');
+  const context = canvas.getContext('2d');
+  const centerX = canvas.width / 2;
+  const centerY = canvas.height / 2;
+  const radius = 3;
+
+  context.beginPath();
+  context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+  context.fillStyle = 'green';
+  context.fill();
+  context.lineWidth = 5;
+  context.strokeStyle = '#003300';
+  context.stroke();
+})
