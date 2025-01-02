2969e79e554f4dbc9fb074f27f786694df3f3711
Glitch (glitch-hello-website) <none>
2025-01-01 20:15:55 -0600

    Updates.

--- script.js
+++ script.js
@@ -1,22 +1,6 @@
 $( document ).ready(function() {
-  function delay(callback, ms) {
-  var timer = 0;
-  return function() {
-    var context = this, args = arguments;
-    clearTimeout(timer);
-    timer = setTimeout(function () {
-      callback.apply(context, args);
-    }, ms || 0);
-  };
-}
-  $('.mirror').keyup(delay(function (e) {
-  $('.'+$(this).attr('class')).text($(this).text());
-}, 500));
-  
- // Dealing with Input width
-let el = document.querySelector(".input-wrap .input");
-let widthMachine = document.querySelector(".input-wrap .width-machine");
-el.addEventListener("keyup", () => {
-  widthMachine.innerHTML = el.value;
-});
+
+  $(".row").on('click',function() {
+    $(this).find(".info").toggle();
+  });
 });
