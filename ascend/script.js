d3f76e07c858119dffac0d75655cae3c0d141d37
Glitch (glitch-hello-website) <none>
2025-08-13 00:18:49 -0500

    Add ascend.

---
+++ ascend/script.js
@@ -0,0 +1,42 @@
+$( document ).ready(function() {
+  $('input').focus(); //sets focus to element
+  $('input').val('')
+  $('input').val('building')
+  
+  const container = document.querySelector("#title");
+const sliders = document.querySelectorAll(".slider");
+const sliderValues = document.querySelectorAll(".output");
+const buttons = document.querySelectorAll(".button");
+
+// Display property values
+for (let i = 0; i < sliders.length; i++) {
+  sliderValues[i].innerHTML = sliders[i].value;
+}
+
+// Update text property and displayed property value for each slider
+sliders.forEach(slider => {
+  const sliderIndex = slider.getAttribute("data-index");
+  slider.oninput = function() {
+      let output = document.querySelector(`.output-${this.id}`);
+      container.style.setProperty(`--${this.id}`, this.value);
+      output.innerHTML = this.value;
+    };
+  });
+
+// Reset text property and update display property value for each slider
+buttons.forEach(button => {
+  const buttonIndex = button.getAttribute("data-index");
+  const resetOutput = document.querySelector(
+    `.output[data-index="${buttonIndex}"]`
+  );
+  const resetSlider = document.querySelector(
+    `.slider[data-index="${buttonIndex}"]`
+  );
+  button.onclick = function() {
+    container.style.removeProperty(`--${resetSlider.id}`);
+    resetOutput.innerHTML = resetSlider.defaultValue;
+    resetSlider.value = resetSlider.defaultValue;
+  };
+});
+
+});
